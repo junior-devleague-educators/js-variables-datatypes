3e45863589e4f818da7eb1395dad793b25e157aa
mellaniesoriano <msoriano808@gmail.com>
2018-01-19 14:34:56 -1000

    added exercise instructions

---
+++ index.js
@@ -0,0 +1,97 @@
+/** Comments are text that will not be executed in your code. Rather comments are for other programmers to read */
+
+// Single line comments start with //
+// Multiple line comments start with /* and ends with */
+
+/** All of the exercise instructions below are commented out.
+ *
+ * WRITE YOUR JAVASCRIPT AFTER EACH SET OF INSTRUCTIONS
+ *
+ */
+
+// example:
+var ateForBreakfast = "Yogurt";
+console.log(ateForBreakfast); // <= This should print out Yogurt
+
+/** #1 Variables with a "String" value
+ *
+ * Declare the following variables:
+ * firstName, lastName, birthPlace, favSubject, favColor, schoolName, schoolDistrict, schoolPrincipal, schoolMascot, schoolColors
+ *
+ * Assign your own string values to each variable and console.log each variable.
+ * Test your code in the terminal using the node repl
+ *
+ * Write your code below
+ *
+ */
+
+/** #2 Variables with a Number value
+ *
+ * Declare the following variables:
+ * favNumber, numOfStudents, classroomNumber, cupsOfCoffee, testScore
+ *
+ * Assign your own number values to each variable and console.log each variable.
+ * Test your code in the terminal using the node repl
+ *
+ * Write your code below
+ */
+
+/** #3 Variables with a Boolean value
+ *
+ * Declare the following variables:
+ * assignmentCompleted, passedTest, attendedClass
+ *
+ * Assign your own boolean values to each variable and console.log each variable.
+ * Test your code in the terminal using the node repl
+ *
+ * Write your code below
+ */
+
+/** #4 Variables with a Boolean value using Comparison and Logic operators
+ *
+ * console.log each variable
+ * Test your code in the terminal using the node repl
+ *
+ */
+
+// Example:
+// Make me true with >
+var booya1 = 3 > 2;
+console.log(booya1);
+
+// Make me False with <
+var booya2;
+
+// Make True with >=
+var booya3;
+
+// Make False with <=
+var booya4;
+
+// Make True with ===
+var booya5;
+
+// Make False with !==
+var booya6;
+
+/** #5 Variables with a Null value
+ *
+ * Declare the following variables:
+ * graduated, nextPresident, universityAttending
+ *
+ * Assign a null value to each variable and console.log each variable
+ * Test your code in the terminal using the node repl
+ *
+ * Write your code below
+ */
+
+/** #6 Variables with an Undefined value
+ *
+ * Declare the following variables:
+ * soccerChampions, enrolled, hasMoney
+ *
+ * Do not assign a value to the variable and console.log each variable
+ * Test your code in the terminal using the node repl
+ *
+ * Write your code below
+ */
