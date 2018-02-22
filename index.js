1b57b1e67ab8fdd5726ed20d8efc016a0f533211
Mellanie Soriano <23709723+mellaniesoriano@users.noreply.github.com>
2018-02-21 15:15:39 -1000

    organized instructions, and updated readme to accommodate for codepen

--- index.js
+++ index.js
@@ -1,57 +1,120 @@
-/** Comments are text that will not be executed in your code. Rather comments are for other programmers to read */
-
-// Single line comments start with //
-// Multiple line comments start with /* and ends with */
-
-/** All of the exercise instructions below are commented out.
- *
- * WRITE YOUR JAVASCRIPT AFTER EACH SET OF INSTRUCTIONS
- *
+/** Comments are text that will not be executed in your code.
+ * Rather comments are for other programmers to read.
+ * 
+ * All of the exercise instructions below are commented out.
+ * Write your JavaScript below each set of instructions.
  */
 
-// example:
+// EXAMPLE:
+// Declare a variable `ateForBreakfast`
 var ateForBreakfast = "Yogurt";
 console.log(ateForBreakfast); // <= This should print out Yogurt
 
-/** #1 Variables with a "String" value
- *
- * Declare the following variables:
- * firstName, lastName, birthPlace, favSubject, favColor, schoolName, schoolDistrict, schoolPrincipal, schoolMascot, schoolColors
+
+
+/** #1 "STRING" DATA TYPE
  *
  * Assign your own string values to each variable and console.log each variable.
- * Test your code in the terminal using the node repl
- *
- * Write your code below
  *
  */
 
-/** #2 Variables with a Number value
- *
- * Declare the following variables:
- * favNumber, numOfStudents, classroomNumber, cupsOfCoffee, testScore
+// 1. Declare a variable `firstName` 
+
+
+// 2. Declare a variable `lastName` 
+
+
+// 3. Declare a variable `hometown` 
+
+
+// 4. Declare a variable `populationOfHometown` 
+
+
+// 5. Declare a variable `shoeSize` 
+
+
+// 6. Declare a variable `howManyMonthsInAYear` 
+
+
+// 7. Declare a variable `hiddenTalent` 
+
+
+// 8. Declare a variable `favoriteBeach` 
+
+
+// 9. Declare a variable `favoriteQuote` 
+
+
+// 10. Declare a variable `favoriteBook` 
+
+
+
+
+/** #2 NUMBER DATA TYPE
  *
  * Assign your own number values to each variable and console.log each variable.
- * Test your code in the terminal using the node repl
- *
- * Write your code below
+ * 
  */
 
-/** #3 Variables with a Boolean value
- *
- * Declare the following variables:
- * assignmentCompleted, passedTest, attendedClass
+// 1. Declare a variable `favoriteNumber` 
+
+
+// 2. Declare a variable `totalNumberOfHawaiianIslands` 
+
+
+// 3. Declare a variable `populationOfHawaii` 
+
+
+// 4. Declare a variable `secondsInAMinute` 
+
+
+// 5. Declare a variable `minutesIn5Hours` 
+
+
+// 6. Declare a variable `hoursIn3Days` 
+
+
+// 7. Declare a variable `totalDaysInALeapYear` 
+
+
+// 8. Declare a variable `numberOfAnimalsIOwn` 
+
+
+// 9. Declare a variable `yearsSinceWW2` 
+
+
+// 10. Declare a variable `ageOfUSA` 
+
+
+
+
+/** #3 BOOLEAN DATA TYPE
  *
  * Assign your own boolean values to each variable and console.log each variable.
- * Test your code in the terminal using the node repl
- *
- * Write your code below
+ * 
  */
 
-/** #4 Variables with a Boolean value using Comparison and Logic operators
+// 1. Declare a variable `assignmentCompleted`
+
+
+// 2. Declare a variable `passedTest`
+
+
+// 3. Declare a variable `attendedClass`
+
+
+// 4. Declare a variable `ateLunch`
+
+
+// 5. Declare a variable `lovesToCode`
+
+
+
+
+/** #4 BOOLEANS WITH COMPARISON AND LOGICAL OPERATORS
  *
  * console.log each variable
- * Test your code in the terminal using the node repl
- *
+ * 
  */
 
 // Example:
@@ -59,39 +122,89 @@ console.log(ateForBreakfast); // <= This should print out Yogurt
 var booya1 = 3 > 2;
 console.log(booya1);
 
-// Make me False with <
+
+// 1. Make me False with <
 var booya2;
 
-// Make True with >=
+
+// 2. Make True with >=
 var booya3;
 
-// Make False with <=
+
+// 3. Make False with <=
 var booya4;
 
-// Make True with ===
+
+// 4. Make True with ===
 var booya5;
 
-// Make False with !==
+
+// 5. Make False with !==
 var booya6;
 
-/** #5 Variables with a Null value
- *
- * Declare the following variables:
- * graduated, nextPresident, universityAttending
+
+// 6. Make True with !=
+var booya6;
+
+
+// 7. Make False with ==
+var booya7;
+
+
+// 8. Make False with >
+var booya8;
+
+
+// 9. Make False with ===
+var booya9;
+
+
+// 10. Make True with !==
+var booya10;
+
+
+
+
+/** #5 NULL DATA TYPE
  *
  * Assign a null value to each variable and console.log each variable
- * Test your code in the terminal using the node repl
- *
- * Write your code below
+ * 
  */
 
-/** #6 Variables with an Undefined value
- *
- * Declare the following variables:
- * soccerChampions, enrolled, hasMoney
+// 1. Declare a variable `graduated`
+
+
+// 2. Declare a variable `nextPresident`
+
+
+// 3. Declare a variable `favoriteSport`
+
+
+// 4. Declare a variable `avgHawaiiTempInWinter`
+
+
+// 5. Declare a variable `phoneNumber`
+
+
+
+
+/** #6 UNDEFINED DATA TYPE
  *
  * Do not assign a value to the variable and console.log each variable
- * Test your code in the terminal using the node repl
  *
- * Write your code below
  */
+
+
+// 1. Declare a variable `gpa`
+
+
+// 2. Declare a variable `groceryList`
+
+
+// 3. Declare a variable `likesPizza`
+
+
+// 4. Declare a variable `favoriteDrink`
+
+
+// 5. Declare a variable `favoriteCar`
